dfcb4a2abfce0e1f0a79c077d1e25855cb74e380
Jason <nyjasonlam@gmail.com>
2021-06-07 22:59:51 -0400

    commented controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,10 @@ const router = require('express').Router()
 const apiRoutes = require('./api')
 const homeRoutes = require('./homeRoutes')
 
+// api routes
 router.use('/api', apiRoutes)
+
+// home routes
 router.use('/', homeRoutes)
 
 module.exports = router
